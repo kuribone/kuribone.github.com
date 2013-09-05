ce43a8153066205dc6ec8d9d3db4a54dd0428a4f
Toshiya KURISHIMA <kuribone@gmail.com>
2013-09-05 18:27:31 +0900

    update haml

--- _plugins/generator_scss.rb
+++ _plugins/generator_scss.rb
@@ -1,20 +1,11 @@
-#
-# Jekyll Generator for SCSS
-#
-# (File paths in this description relative to jekyll project root directory)
-# Place this file in ./_plugins
-# Place .scss files in ./_scss
-# Compiles .scss files in ./_scss to .css files in whatever directory you indicated in your config
-# Config file placed in ./_sass/config.rb
-#
- 
 require 'sass'
+require 'haml'
 require 'pathname'
 require 'compass'
 require 'compass/exec'
  
 module Jekyll
- 
+
   class CompassGenerator < Generator
     safe true
  
@@ -22,6 +13,7 @@ module Jekyll
       Dir.chdir File.expand_path('../_sass', File.dirname(__FILE__)) do
         Compass::Exec::SubCommandUI.new(%w(compile)).run!
       end
+
     end
     
   end
--- _sass/config.rb
+++ _sass/config.rb
@@ -4,6 +4,7 @@
 http_path = "/"
 css_dir = "../css"
 sass_dir = "./"
+haml_dir = "../_haml"
 images_dir = "../img"
 javascripts_dir = "../js"
 
